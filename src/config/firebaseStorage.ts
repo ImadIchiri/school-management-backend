5e158f9143c87b2424973627a9ec809bb9006788
Ichiri Imad <ichiri02imad@gmail.com>
2025-11-18 11:05:17 +0100

    Remove Firebase Settings - It will be created by Oumaima

--- src/config/firebaseStorage.ts
+++ src/config/firebaseStorage.ts
@@ -1,34 +1,34 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Firestore setup
-const {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  BUCKET,
-  SENDER_ID,
-  APP_ID,
-  MEAUREMENT_ID,
-} = process.env;
+// const {
+//   API_KEY,
+//   AUTH_DOMAIN,
+//   PROJECT_ID,
+//   BUCKET,
+//   SENDER_ID,
+//   APP_ID,
+//   MEAUREMENT_ID,
+// } = process.env;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: API_KEY as string,
-  authDomain: AUTH_DOMAIN as string,
-  projectId: PROJECT_ID as string,
-  storageBucket: BUCKET as string,
-  messagingSenderId: SENDER_ID as string,
-  appId: APP_ID as string,
-  measurementId: MEAUREMENT_ID as string, // optional
-};
+// const firebaseConfig = {
+//   apiKey: API_KEY as string,
+//   authDomain: AUTH_DOMAIN as string,
+//   projectId: PROJECT_ID as string,
+//   storageBucket: BUCKET as string,
+//   messagingSenderId: SENDER_ID as string,
+//   appId: APP_ID as string,
+//   measurementId: MEAUREMENT_ID as string, // optional
+// };
 
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-export const firestore = getStorage(firebaseApp);
+// export const firebaseApp = initializeApp(firebaseConfig);
+// export const firestore = getStorage(firebaseApp);
 // const analytics = getAnalytics(firebaseApp);
--- src/modules/ressource/ressourceRoutes.ts
+++ src/modules/ressource/ressourceRoutes.ts
@@ -1,18 +1,1 @@
-import express from "express";
-import multer from "multer";
-import * as ressourceController from "./ressourceController";
 
-// Multer setup
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-const ressourcRrouter = express.Router();
-
-// Upload Single file named: ressource
-ressourcRrouter.post(
-  "/upload",
-  upload.single("ressource"),
-  ressourceController.createRessource
-);
-
-export default ressourcRrouter;
